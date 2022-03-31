cbc0140dee0d5fae7f211c2c607decb09459c5fb
Жвакин Дмитрий <crimsnemptnss@yandex.ru>
2022-03-31 12:53:26 +0500

    create custom select

---
+++ src/app/select/select.component.ts
@@ -0,0 +1,45 @@
+import { Component, forwardRef, Input } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from "@angular/forms";
+import { selectOption } from "../../types/controlsConfig.type";
+
+@Component({
+  selector: 'MySelect',
+  templateUrl: './select.component.html',
+  styleUrls: ['./select.component.css'],
+  providers: [{
+    provide: NG_VALUE_ACCESSOR,
+    useExisting: forwardRef(() => SelectComponent),
+    multi: true
+  }]
+})
+export class SelectComponent implements ControlValueAccessor {
+  @Input()
+  options?: selectOption[] = [];
+
+  private _value: string;
+
+  get value() {
+    return this._value;
+  }
+
+  @Input()
+  set value(v: string) {
+    this._value = v;
+    this.onChange(this._value);
+  }
+
+  constructor() { }
+
+  onChange(_: any) {}
+
+  public writeValue(value: any) {
+    this.value = value;
+  }
+
+  public registerOnChange(fn: any) {
+    this.onChange = fn;
+  }
+
+  public registerOnTouched(fn: any) {}
+
+}
