382d34647e349f6850a06e7aa28c83f79f52a344
Жвакин Дмитрий <crimsnemptnss@yandex.ru>
2022-03-31 12:58:18 +0500

    update app (add changes previous commits)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,38 @@
 import { Component } from '@angular/core';
+import { controlsConfig, inputTypes } from "../types/controlsConfig.type";
+
+const controlsConfig: controlsConfig = {
+  name: {
+    initValue: '',
+    type: inputTypes.inputtext,
+    validator: (value => Boolean(value && value.length <= 255)),
+    inputTitle: 'Имя',
+    inputPlaceholder: 'Введите имя...'
+  },
+  type: {
+    initValue: '',
+    type: inputTypes.select,
+    selectOptions: [
+      {
+        title: 'Физическое лицо',
+        value: 1
+      },
+      {
+        title: 'Юридическое лицо',
+        value: 2
+      }
+    ],
+    validator: (value => Boolean(value)),
+    inputTitle: 'Тип',
+  },
+  number: {
+    initValue: '',
+    type: inputTypes.inputtext,
+    validator: (value => Boolean(value && value.toString().length === 13)),
+    inputTitle: 'Номер',
+    inputPlaceholder: 'Введите номер...'
+  }
+};
 
 @Component({
   selector: 'app-root',
@@ -6,5 +40,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  modalAddConsumerFlag = true;
+  controlsConfig: controlsConfig = controlsConfig;
+
+  constructor() {
+    this.closeModalAddConsumer = this.closeModalAddConsumer.bind(this);
+  }
+
+  openModalAddConsumer(): void {
+    this.modalAddConsumerFlag = true;
+  }
+
+  closeModalAddConsumer(): void {
+    this.modalAddConsumerFlag = false;
+  }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { TableComponent } from './table/table.component';
 import { CommonModule } from "@angular/common";
+import { ModalComponent } from './modal/modal.component';
+import { InputComponent } from './input/input.component';
+import { FormComponent } from './form/form.component';
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { SelectComponent } from './select/select.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TableComponent
+    TableComponent,
+    ModalComponent,
+    InputComponent,
+    FormComponent,
+    SelectComponent
   ],
   imports: [
     BrowserModule,
-    CommonModule
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
